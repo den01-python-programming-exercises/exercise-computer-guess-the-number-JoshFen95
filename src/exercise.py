7c9ab622acd7c6660b43a5de8f597fd9854c1486
JoshFen95 <jfennell@companieshouse.gov.uk>
2020-05-18 14:00:15 +0100

    attempt

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,26 @@
+from random import randint
+
+
 def main():
-    #write your code below this line
+    # write your code below this line
+    number = randint(1, 10)
+    print("Computer guessed " + str(number))
+    while True:
+
+       
+        user_input = input("higher, lower or correct: ")
+
+        if user_input == "correct":
+            print("Computer guessed correctly:")
+            break
+        elif user_input == "higher":
+            new_guess = randint(number, 10)
+            print("Computer guessed: " + str(new_guess))
+        elif user_input == "lower":
+            new_guess = randint(1, number)
+            print("Computer guessed: " + str(new_guess))
+
+
 
 if __name__ == '__main__':
     main()
